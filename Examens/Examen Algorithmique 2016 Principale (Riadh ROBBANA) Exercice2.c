f2e671fc8c19ee666e3c85c56c3b107d9d3be29d
SNS_ANS <anissnoussi@outlook.com>
2018-05-24 10:59:36 +0000

    MPI

--- Examens/Examen Algorithmique 2016 Principale (Riadh ROBBANA) Exercice2.c
+++ Examens/Examen Algorithmique 2016 Principale (Riadh ROBBANA) Exercice2.c
@@ -22,8 +22,9 @@ void detruireRecursive(arbre a){
 
 
 //fonction itérative pour detruire une arbre binaire
+//version 1: on va supprimer le plus petit element jusqu'a detriure l'arbre
 
-void detruireIterative(arbre a){
+void detruireIterativeV1(arbre a){
 	arbre courant;
 	int arbreTerminee = 0;
 	while(!arbreTerminee)
@@ -63,5 +64,75 @@ void detruireIterative(arbre a){
 		{
 			arbreTerminee = 1;
 		}
+	}
+}
+
+
+//fonction itérative pour detruire une arbre binaire
+//version 2: on va utiliser un file pour se faire
+//donc on doit defenir les fonctions necessaires
+
+typedef struct File{
+	arbre a;
+	struct File* suiv;
+}File;
+typedef File* file;
+
+file teteDuFile ;
+
+file creeVide(){
+	return NULL;
+}
+int fileEstVide(file f){
+	return(f == NULL);
+}
+void enfiler (file* f, arbre a){
+	file nvFile = creeVide();
+	if(!(nvFile = (file)malloc(sizeof(File)))){
+		printf("erreur d'allocation");
+		exit(0);
+	}
+	nvFile->a = a;
+	nvFile->suiv=NULL;
+	
+	if(fileEstVide(*f)){
+		*f = nvFile;
+		return;
+	}
+	file courant = *f;
+	while(!fileEstVide(courant->suiv)){
+		courant = courant->suiv;
+	}
+	courant->suiv = nvFile;
+}
+arbre  getTete(file f){
+	if(fileEstVide(f)){
+		return NULL;
+	}
+	return(f->a);
+}
+void defiler(file *f){
+	file fileAux = *f;
+	*f = *f->suiv;
+	free(fileAux);
+}
+
+//fonction de destruction de l'arbre
+void detruireIterativeV2(arbre a){
+	file nvFile =creeVide();
+	enfiler(&nvFile,a);
+	while(!fileEstVide(nvFile)){
+		arbre aux, aux1, aux2;
+		aux = getTete(nvFile);
+		aux1 =aux->fd;
+		aux2 =aux->fg;
+		if(aux1){
+			enfiler(&nvFile,aux1);
+		}
+		if(aux2){
+			enfiler(&nvFile,aux2);
+		}
+		free(aux);
+		defiler(&nvFile);
 	}
 }
